f3e29958d7b4a1c341dac7ff09677169c31d0457
Sudershan <gksudershan@gmail.com>
2022-10-23 21:04:25 +0530

    Add a useful comment

--- user_input.sh
+++ user_input.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-read -p "Enter your name & age: " NAME AGE
+read -p "Enter your name & age: " NAME AGE # -p option allows to specify a prompt message
 
 echo "Your name is $NAME and age is $AGE"
